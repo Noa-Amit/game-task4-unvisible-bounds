824bba71d3553c85f67a299ddd0d295d36c1c637
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2020-11-17 11:11:18 +0200

    Initial commit

---
+++ Assets/Scripts/3-collisions/DestroyOtherOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOtherOnTrigger : MonoBehaviour
+{
+    [SerializeField] string triggeringTag;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# game-task4-unvisible-bounds
+ 
